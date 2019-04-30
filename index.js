445c71621db4329d72c1c9bf9d1cf29890a56b73
Luke Childs <lukechilds123@gmail.com>
2019-04-30 14:33:11 +0700

    Don't use modern function syntax

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 'use strict';
 
-const isPromise = input => (
-	input instanceof Promise ||
-	(
-		input !== null &&
-		typeof input === 'object' &&
-		typeof input.then === 'function' &&
-		typeof input.catch === 'function'
-	)
-);
+function isPromise(input) => {
+	return (
+		input instanceof Promise ||
+		(
+			input !== null &&
+			typeof input === 'object' &&
+			typeof input.then === 'function' &&
+			typeof input.catch === 'function'
+		)
+	);
+}
 
 module.exports = isPromise;
 // TODO: Remove this for the next major release
